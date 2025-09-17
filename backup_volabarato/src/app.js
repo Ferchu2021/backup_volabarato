06b63bed3f131b79048ed31bed033a743dba6996
Ferchu2021 <82846952+Ferchu2021@users.noreply.github.com>
2025-09-17 00:08:43 -0300

    conexión Mongoose con mensaje y manejo de errores

--- backup_volabarato/src/app.js
+++ backup_volabarato/src/app.js
@@ -15,7 +15,14 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Conectado a MongoDB Atlas'))
+.catch(err => console.error('Error de conexión:', err));
 
 app.use('/api/paquete', paqueteRoutes);
 app.use('/api/user', userRoutes);
